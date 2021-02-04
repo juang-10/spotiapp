83981ff110b8f70ed20311a0f1ec695f3beeecc4
Juan David Gonzalez <judagosa8520@gmail.com>
2021-02-04 17:36:04 -0500

    cambios archivos de la carpeta app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,9 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+//Importar rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -18,7 +24,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot( ROUTES, { useHash: true} )
   ],
   providers: [],
   bootstrap: [AppComponent]
