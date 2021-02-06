78c6191257c8849586968e5ac37ca198664558d5
Juan David Gonzalez <judagosa8520@gmail.com>
2021-02-06 08:42:31 -0500

    Mostrando canciones y  nombre de artistas

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,13 +13,10 @@ export class SpotifyService {
   getNewReleases() {
 
   const headers = new HttpHeaders({
-    'Authorization': 'Bearer BQC9BtBpvIQyn5ENBLOvM-iMfyZaIhUZmutLboide2jekokpoCp9vXR_gRrTrcd2NOujHjTSYq7A4mUecaU'
+    'Authorization': 'Bearer BQCw1gtuGGfxCdUsCFosjuH0sjq60y_67yB3fBz0YblwOGT61Kt7JGmUT4u9dkD032-2kT2dVq16VsTUtLk'
   });
 
-    this.http.get('	https://api.spotify.com/v1/browse/new-releases?limit=20',{headers}) 
-      .subscribe( data => {
-        console.log(data);
-      });
-
+    return this.http.get('	https://api.spotify.com/v1/browse/new-releases?country=CO&limit=6',{headers}); 
+  
   }
 }
